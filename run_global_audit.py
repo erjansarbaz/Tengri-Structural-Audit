dc5f0dc3031003f987b2c94ac0c94b68c1cb5215
erjansarbaz <relaxout892102@gmail.com>
2026-07-13 16:28:53 +0500

    Update run_global_audit.py

--- run_global_audit.py
+++ run_global_audit.py
@@ -15,7 +15,10 @@ etalon_tension = get_total_tension('NC_004102.fasta')
 
 print(f"{'Файл':<25} | {'Напряжение':<15} | {'Сходство с NC_004102':<20}")
 print("-" * 65)
-
+# Проверка: есть ли хоть один файл для анализа
+if not files:
+    print("Ошибка: Не найдено файлов .fasta. Положи их в папку со скриптом.")
+    return
 for file in files:
     tension = get_total_tension(file)
     # Относительное сходство
